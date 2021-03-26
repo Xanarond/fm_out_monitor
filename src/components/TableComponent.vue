f61358abeb7b91b5c5ed6cd544ad22ea69b14d5d
Vlad <vlakos2895@gmail.com>
2021-03-26 12:05:22 +0300

    Arr lock & Translate

--- src/components/TableComponent.vue
+++ src/components/TableComponent.vue
@@ -3,7 +3,7 @@
     <b-container fluid="">
       <b-row>
         <b-col>
-          <h2 class="text-left title" style="color: #fff">Day shift</h2>
+          <h2 class="text-left title" style="color: #fff">Дневная смена</h2>
           <b-table
               class="out_table mt-1"
               :items="day_shift"
@@ -14,7 +14,7 @@
           ></b-table>
         </b-col>
         <b-col>
-          <h2 class="text-left title" style="color: #fff">Night shift</h2>
+          <h2 class="text-left title" style="color: #fff">Ночная смена</h2>
           <b-table
               class="out_table mt-1"
               :items="night_shift"
@@ -37,58 +37,100 @@ export default {
 
   name: "TableComponent",
   created() {
-    let target = Array(12).fill(250)
-
+    let target = Array(12).fill(292)
+    target[12] = 3575
 
     let time_zone = []
 
     for (let i = 0; i <= 23; i++) {
       time_zone[i] = i <= 9 ? `0${i}:00` : `${i}:00`;
     }
+
     let day_shift = time_zone.slice(8, 20)
     let night_shift = time_zone.slice(20, 24).concat(time_zone.slice(0, 8))
-
+    day_shift[12] = 'Total'
+    night_shift[12] = 'Total'
     let rand_int = []
 
-    for (let i = 0; i <= 11; i++) {
-      let el = Math.floor(Math.random() * 1000)
-      rand_int[i] = el
+    //for (let i = 0; i <= 11; i++) {
+    //let el = Math.floor(Math.random() * 1000)
+    rand_int[0] = 122
+    rand_int[1] = 0
+    rand_int[2] = 0
+    rand_int[3] = 0
+    rand_int[4] = 0
+    rand_int[5] = 0
+    rand_int[6] = 0
+    rand_int[7] = 0
+    rand_int[8] = 0
+    rand_int[9] = 0
+    rand_int[10] = 0
+    rand_int[11] = 0
+    //rand_int[12] = rand_int[0] + rand_int[1] + rand_int[2] + rand_int[3] + rand_int[4] + rand_int[5] + rand_int[6] + rand_int[7] + rand_int[8] + rand_int[9] + rand_int[10] + rand_int[11]
+    var sum=0;
+    for(var i=0;i<=11;i++){
+      sum = sum + parseInt(rand_int[i]);
+      rand_int[12] = sum
     }
+    //}
 
     let rand_int2 = []
 
-    for (let i = 0; i <= 11; i++) {
-      let el2 = Math.floor(Math.random() * 1000)
-      rand_int2[i] = el2
+    //for (let i = 0; i <= 11; i++) {
+    //let el2 = Math.floor(Math.random() * 1000)
+    rand_int2[0] = 50
+    rand_int2[1] = 70
+    rand_int2[2] = 239
+    rand_int2[3] = 288
+    rand_int2[4] = 57
+    rand_int2[5] = 2
+    rand_int2[6] = 106
+    rand_int2[7] = 18
+    rand_int2[8] = 373
+    rand_int2[9] = 137
+    rand_int2[10] = 86
+    rand_int2[11] = 208
+    //rand_int2[12] = rand_int2[0] + rand_int2[1] + rand_int2[2] + rand_int2[3] + rand_int2[4] + rand_int2[5] + rand_int2[6] + rand_int2[7] + rand_int2[8] + rand_int2[9] + rand_int2[10] + rand_int2[11]
+    //}
+    var sum2=0;
+    for(i=0;i<=11;i++){
+      sum2 = sum2 + parseInt(rand_int2[i]);
+      rand_int2[12] = sum2
     }
 
-
     let status1 = []
-    for (let i = 0; i <= 12; i++) {
+    //let percent = 0.85
+    for (let i = 0; i <= 11; i++) {
       if (target[i] < rand_int[i]) {
-        status1[i] = 'Excellent';
-      } else if (target[i] === rand_int[i]) {
-        status1[i] = 'Not bad';
+        status1[i] = 'Отлично';
+      } else if (rand_int[i] > 248) {
+        status1[i] = 'Норма';
       } else {
-        status1[i] = 'Bad';
+        status1[i] = 'Плохо';
+      }
+      if (rand_int[i] ==0) {
+        status1[i] = '-';
       }
     }
 
     let status2 = []
-    let percent = 0.85
-    for (let i = 0; i <= 12; i++) {
+    //let percent = 0.85
+    for (let i = 0; i <= 11; i++) {
       if (target[i] < rand_int2[i]) {
-        status2[i] = 'Excellent';
-      } else if (target[i] < rand_int2[i] * percent) {
-        status2[i] = 'Not bad';
-      } else if (target[i] > 230) {
-        status2[i] = 'Bad';
+        status2[i] = 'Отлично';
+      } else if (rand_int2[i] > 248) {
+        status2[i] = 'Норма';
+      } else {
+        status2[i] = 'Плохо';
+      }
+      if (rand_int2[i] ==0) {
+        status2[i] = '-';
       }
     }
 
     let day_arr = []
 
-    for (let i = 0; i <= 11; i++) {
+    for (let i = 0; i <= 12; i++) {
       let obj = {
         time: day_shift[i],
         result: rand_int[i],
@@ -101,7 +143,7 @@ export default {
 
     let night_arr = []
 
-    for (let i = 0; i <= 11; i++) {
+    for (let i = 0; i <= 12; i++) {
       let obj = {
         time: night_shift[i],
         result: rand_int2[i],
@@ -115,9 +157,9 @@ export default {
     this.$data.night_shift = night_arr
 
     $(function () {
-      $('td:contains("Bad")').parent().css('background', '#ff0000')
-      $('td:contains("Excellent")').parent().css('background', '#00AF50')
-      $('td:contains("Not bad")').parent().css('background', '#0071C0')
+      $('td:contains("Плохо")').parent().css('background', '#ff0000')
+      $('td:contains("Отлично")').parent().css('background', '#00AF50')
+      $('td:contains("Норма")').parent().css('background', '#0071C0')
 
     });
 
@@ -151,10 +193,10 @@ export default {
 
 @media screen and (min-width: 2280px) {
   .out_table {
-    font-size: 80px;
+    font-size: 100px;
   }
   .title{
-    font-size: 80px;
+    font-size: 100px;
   }
 }
 
