bd24d50679a892020c0fb7d0021e7ec592df14dd
Vlad <vlakos2895@gmail.com>
2021-03-26 16:55:16 +0300

    Add Total counts & some refactor
    1) Добавил таблицы с расчетом тоталов
    2) Небольшие правки и сокращения кода
    3) добавление коментариев

--- src/components/TableComponent.vue
+++ src/components/TableComponent.vue
@@ -3,7 +3,7 @@
     <b-container fluid="">
       <b-row>
         <b-col>
-          <h2 class="text-left title" style="color: #fff">Day shift</h2>
+          <h2 class="text-left title">Дневная смена</h2>
           <b-table
               class="out_table mt-1"
               :items="day_shift"
@@ -14,11 +14,33 @@
           ></b-table>
         </b-col>
         <b-col>
-          <h2 class="text-left title" style="color: #fff">Night shift</h2>
+          <h2 class="text-left title">Ночная смена</h2>
           <b-table
               class="out_table mt-1"
               :items="night_shift"
               :fields="fields"
+              :table-variant="tableVariant"
+              :bordered="true"
+              :head-variant="col"
+          ></b-table>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col>
+          <b-table
+              class="out_table"
+              :items="total_d"
+              :fields="total_fields"
+              :table-variant="tableVariant"
+              :bordered="true"
+              :head-variant="col"
+          ></b-table>
+        </b-col>
+        <b-col>
+          <b-table
+              class="out_table"
+              :items="total_n"
+              :fields="total_fields"
               :table-variant="tableVariant"
               :bordered="true"
               :head-variant="col"
@@ -33,37 +55,68 @@
 
 import $ from 'jquery'
 
+
 export default {
 
   name: "TableComponent",
   created() {
+    // массив целевых значений
     let target = Array(12).fill(250)
 
+    // задаем массивы для временных промежутков
     let time_zone = []
 
     for (let i = 0; i <= 23; i++) {
       time_zone[i] = i <= 9 ? `0${i}:00` : `${i}:00`;
     }
+
     let day_shift = time_zone.slice(8, 20)
     let night_shift = time_zone.slice(20, 24).concat(time_zone.slice(0, 8))
 
     let rand_int = []
 
     for (let i = 0; i <= 11; i++) {
-      let el = Math.floor(Math.random() * 1000)
-      rand_int[i] = el
+      rand_int[i] = Math.floor(Math.random() * 1000)
     }
 
     let rand_int2 = []
 
     for (let i = 0; i <= 11; i++) {
-      let el2 = Math.floor(Math.random() * 1000)
-      rand_int2[i] = el2
+      rand_int2[i] = Math.floor(Math.random() * 1000)
     }
 
+    // суммы для результатов
+    let total_day = rand_int.reduce((sum, cur) => {
+      return sum + cur
+    }, 0);
 
+    let total_night = rand_int2.reduce((sum, cur) => {
+      return sum + cur
+    }, 0);
+
+    let target_sum = target.reduce((sum, cur) => {
+      return sum + cur
+    }, 0);
+
+    let sum_day = []
+    let sum_night = []
+
+    let day_t = {
+      target: target_sum,
+      total: total_day,
+    }
+    sum_day.push(day_t)
+
+    let night_t = {
+      target: target_sum,
+      total: total_night
+    }
+    sum_night.push(night_t)
+
+    //рассчет статусов
     let status1 = []
-    for (let i = 0; i <= 12; i++) {
+
+    for (let i = 0; i <= 11; i++) {
       if (target[i] < rand_int[i]) {
         status1[i] = 'Отлично';
       } else if (rand_int[i] > 200) {
@@ -74,7 +127,7 @@ export default {
     }
 
     let status2 = []
-    for (let i = 0; i <= 12; i++) {
+    for (let i = 0; i <= 11; i++) {
       if (target[i] < rand_int2[i]) {
         status2[i] = 'Отлично';
       } else if (rand_int2[i] > 200) {
@@ -84,6 +137,7 @@ export default {
       }
     }
 
+    // упаковка обьектов статусов в массив
     let day_arr = []
 
     for (let i = 0; i <= 11; i++) {
@@ -108,25 +162,28 @@ export default {
       night_arr.push(obj);
     }
 
-    this.$data.day_shift = day_arr
-    this.$data.night_shift = night_arr
-
     $(function () {
       $('td:contains("Плохо")').parent().css('background', '#ff0000')
       $('td:contains("Отлично")').parent().css('background', '#00AF50')
       $('td:contains("Норма")').parent().css('background', '#0071C0')
     });
 
+    this.day_shift = day_arr
+    this.night_shift = night_arr
+    this.total_d = sum_day
+    this.total_n = sum_night
+
   },
   data() {
     return {
       fields: ['time', 'target', 'result', 'status'],
+      total_fields: ['target', 'total'],
       tableVariant: 'dark',
       day_shift: [],
       night_shift: [],
       col: 'dark',
-      x: 2,
-      y: 3
+      total_d: [],
+      total_n: []
     }
   },
   mounted() {
@@ -137,9 +194,9 @@ export default {
 </script>
 <style scoped>
 
-@media screen and (min-width: 2280px) {
+@media screen and (min-width: 2160px) {
   .out_table {
-    font-size: 80px;
+    font-size: 70px;
   }
 
   .title {
@@ -147,13 +204,14 @@ export default {
   }
 }
 
-@media screen and (min-width: 1280px) {
+@media screen and (max-width: 1000px) {
   .out_table {
     font-size: 30px;
   }
 
   .title {
     font-size: 30px;
+    color: #fff;
   }
 }
 
