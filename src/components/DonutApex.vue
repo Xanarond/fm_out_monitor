eaab778c5ba25b4174feea4fe179b64d7637d770
Vlad Kostyukhin <kostyukhin.v@EU.corp.samsungelectronics.net>
2021-09-15 13:06:04 +0300

    Add MLPage

---
+++ src/components/DonutApex.vue
@@ -0,0 +1,134 @@
+<template>
+  <div id="chart">
+    <apexchart type="donut" :options="chartOptions" :series="series"></apexchart>
+  </div>
+</template>
+
+<script>
+import VueApexCharts from "vue-apexcharts";
+
+export default {
+  name: "DonutApex",
+  props: ['values', 'labels'],
+  components: {
+    apexchart: VueApexCharts,
+  },
+  data() {
+    return {
+      series: this.$props.values,
+      chartOptions: {
+        chart: {
+          type: 'donut',
+          width: 380,
+          height: 380,
+          animations: {
+            enabled: true,
+            easing: 'easeinout',
+            speed: 1000,
+            animateGradually: {
+              enabled: true,
+              delay: 150,
+            },
+            dynamicAnimation: {
+              enabled: true,
+              speed: 350,
+            },
+          },
+        },
+        legend: {
+          position: 'right',
+          floating: false,
+          fontSize: '28px',
+          fontFamily: 'Helvetica, Arial',
+          fontWeight: 400,
+          horizontalAlign: 'center',
+          labels: {
+            colors: '#fff',
+            useSeriesColors: false,
+          },
+          markers: {
+            width: 12,
+            height: 12,
+            strokeWidth: 0,
+            strokeColor: '#fff',
+            fillColors: ['#0037fc', '#ff0000', '#f1ff00'],
+            radius: 12,
+            offsetX: 0,
+            offsetY: 0,
+          },
+        },
+        fill: {
+          colors: ['#0037fc', '#ff0000', '#f1ff00'],
+        },
+        labels: this.$props.labels,
+        dataLabels: {
+          position: 'top',
+          style: {
+            fontSize: '22px',
+            fontFamily: 'Helvetica, Arial, sans-serif',
+            fontWeight: 'bold',
+            colors: ['#fff'],
+          },
+        },
+        stroke: {
+          width: 0,
+        },
+        tooltip: {
+          enabled: false,
+        },
+        theme: {
+          monochrome: {
+            enabled: true,
+            color: '#ffffff',
+          },
+        },
+        noData: {
+          text: "Нет данных",
+          align: 'center',
+          verticalAlign: 'middle',
+          offsetX: 0,
+          offsetY: 0,
+          style: {
+            color: '#fff',
+            fontSize: '23px',
+          },
+        },
+        plotOptions: {
+          pie: {
+            donut: {
+              size: '45%',
+              labels: {
+                show: true,
+                value: {
+                  show: true,
+                  fontSize: '32px',
+                  fontFamily: 'Helvetica, Arial, sans-serif',
+                  fontWeight: 300,
+                  color: '#fff',
+                  offsetY: 16,
+                  glob(w) {
+                    return w.globals.seriesTotals;
+                  },
+                },
+                total: {
+                  showAlways: true,
+                  show: true,
+                  label: 'Total',
+                  fontSize: '32px',
+                  fontFamily: 'Helvetica, Arial, sans-serif',
+                  fontWeight: 600,
+                  color: '#ffffff',
+                },
+              },
+            },
+          },
+        },
+      },
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/MLPage.vue
+++ src/pages/MLPage.vue
@@ -2,35 +2,63 @@
   <div id="chart">
     <b-container fluid="">
       <b-row class="justify-content-center mt-2">
-        <div class="d-inline-block  mt-auto mb-auto" style="color: #fff">Period:</div>
-        <VueCtkDateTimePicker  :dark=true :label="'Select Date'" :noButton=true
-                              :noHeader=true :noValueToCustomElem=true
-                              :onlyDate=true class="justify-content-start d-inline-block mt-auto mb-auto"
-                              formatted="L"
-                              style="width: 380px;margin:0 5px 5px;"/>
-        <button class="d-inline-block ml-1 mt-auto mb-auto btn btn-secondary text-white mr-2" type="button"
-                >Refresh
+        <div class="d-inline-block mt-auto mb-auto" style="color: #fff">
+          Period:
+        </div>
+        <VueCtkDateTimePicker
+            v-model="period.cur_date"
+            :dark="true"
+            :label="'Select Date'"
+            :noButton="true"
+            :noHeader="true"
+            :noValueToCustomElem="true"
+            :onlyDate="true"
+            class="justify-content-start d-inline-block mt-auto mb-auto"
+            formatted="L"
+            style="width: 380px; margin: 0 5px 5px"
+        />
+        <button
+            class="
+            d-inline-block
+            ml-1
+            mt-auto
+            mb-auto
+            btn btn-secondary
+            text-white
+            mr-2
+          "
+            type="button"
+            v-on:click="getCountData()"
+        >
+          Refresh
         </button>
       </b-row>
       <b-row class="ml-2 mt-3 mr-2">
-        <b-col class="p-2" style="background: #29292d; border: #fff solid 1px">
+        <b-col></b-col>
+        <b-col fluid="md" cols="4" class="p-2" style="background: #29292d; border: #fff solid 1px; width: 300px;">
           <h2 class="text-right">Manifest - Normal D/O</h2>
-          <DonutChart v-bind:values="[20, 30, 500]" v-bind:labels="['Completed', 'Not Packed', 'Urgent']" v-bind:background-color="['#0037fc', '#ff0000', '#f1ff00']"/>
+          <DonutApex :values="Object.values(this.$data.normal_lines)"
+                     :key="Object.keys(this.$data.normal_lines)" label="Completed"/>
         </b-col>
-        <b-col class="p-2" style="background: #29292d; border: #fff solid 1px">
+        <b-col fluid="md" cols="4" class="p-2" style="background: #29292d; border: #fff solid 1px">
           <h2 class="text-right">Manifest - Urgent D/O</h2>
-          <DonutChart v-bind:values="[20, 30]" v-bind:labels="['Completed', 'Not Packed']" v-bind:background-color="['#0037fc', '#ff0000']"/>
+          <DonutApex :values="Object.values(this.$data.urgent_manifests)" :key="Object.keys(this.$data.urgent_manifests)"/>
         </b-col>
+        <b-col></b-col>
       </b-row>
       <b-row class="ml-2 mr-2">
-        <b-col class="p-2" style="background: #29292d; border: #fff solid 1px">
+        <b-col></b-col>
+        <b-col class="p-2" cols="4" style="background: #29292d; border: #fff solid 1px">
           <h2 class="text-right">Lines - Normal D/O</h2>
-          <DonutChart v-bind:values="[20, 30, 500]" v-bind:labels="['Completed', 'Not Packed', 'Urgent']" v-bind:background-color="['#0037fc', '#ff0000', '#f1ff00']"/>
+          <DonutApex :values="this.$data.normal_lines"
+                     :key="this.$data.normal_lines" label="Completed"/>
         </b-col>
-        <b-col class="p-2" style="background: #29292d; border: #fff solid 1px">
+        <b-col class="p-2" cols="4" style="background: #29292d; border: #fff solid 1px">
           <h2 class="text-right">Lines - Urgent D/O</h2>
-          <DonutChart v-bind:values="[20, 30]" v-bind:labels="['Completed', 'Not Packed']" v-bind:background-color="['#0037fc', '#ff0000']"/>
+          <DonutApex :values="Object.values(this.$data.urgent_lines)"
+                     :key="Object.keys(this.$data.urgent_lines)"/>
         </b-col>
+        <b-col></b-col>
       </b-row>
       <b-row class="mt-2">
         <b-col id="backlog_col">
@@ -42,58 +70,72 @@
 </template>
 
 <script>
-
-import DonutChart from "@/components/DonutChart";
 import moment from "moment";
 import http from "@/http-common";
+// import DonutApex from "../components/DonutApex";
 
 export default {
   name: "MLPage",
   components: {
-    DonutChart,
+    // DonutApex,
   },
   data() {
     return {
       period: {
         cur_date: moment()
-          .format('YYYY-MM-DD'),
+          .format("YYYY-MM-DD"),
         past_date: moment(this.cur_date)
-          .subtract(1, 'day')
-          .format('YYYY-MM-DD'),
+          .subtract(1, "day")
+          .format("YYYY-MM-DD"),
       },
+      normal_lines: [],
+      urgent_lines: [],
+      normal_manifests: [],
+      urgent_manifests: [],
+      colors: ['#0037fc', '#ff0000', '#f1ff00'],
     };
   },
   methods: {
     getCountData() {
-      http.get('/getMLNormal', {
-        /* params: {
+      http.get("/getMLStatus", {
+        params: {
           cur_date: this.$data.period.cur_date.slice(0, 10),
-          past_date: moment(this.$data.period.cur_date.slice(0, 10))
-            .subtract(1, 'day')
-            .format('YYYY-MM-DD'),
-        }, */
+        },
       })
         .then((res) => {
-          // результирующий массив из БД
-          // const counts = [];
-
-          console.log(res.data);
-
-          /* res.data.shift_stat.forEach(((item) => {
-            shifts.push(item.result);
-          }));
-
-          this.series = [{
-            data: shifts.slice(8, 20),
-          }];
-
-          // суммы для результатов
-          const com = shifts.slice(8, 20)
-            .reduce((sum, cur) => sum + cur, 0);
-          this.complete = new Intl.NumberFormat('en-US').format(com);
-          this.progress = `${Math.floor((com / this.total) * 100)}%`; */
+          // const norm_l = ;
+          /* const urg_l = res.data.line_urgent[0];
+          const urg_m = res.data.urgent_manifests[0];
+          const norm_m = res.data.normal_manifests[0]; */
+          const norm_l = Object.values(res.data.line_norm[0]);
+          const urg_l = Object.values(res.data.line_urgent[0]);
+          this.normal_lines = norm_l;
+          this.urgent_lines = urg_l;
+          console.log(this.normal_lines);
+          /* this.normal_lines = { ...norm_l };
+          this.urgent_lines = { ...urg_l };
+          this.normal_manifests = { ...norm_m };
+          this.urgent_manifests = { ...urg_m }; */
         })
-        .catch((e) => e === 'Нет ответа от сервера');
+        .catch((e) => e === "Нет ответа от сервера");
+    },
+    getRandom() {
+      const urg_l = [];
+      const norm_l = [];
+      const urg_m = [];
+      const norm_m = [];
+
+      for (let i = 0; i <= 2; i++) {
+        urg_l[i] = Math.floor(Math.random() * 100);
+        norm_l[i] = Math.floor(Math.random() * 100);
+        urg_m[i] = Math.floor(Math.random() * 100);
+        norm_m[i] = Math.floor(Math.random() * 100);
+      }
+
+      this.normal_lines = { ...norm_l };
+      this.urgent_lines = { ...urg_l };
+      this.normal_manifests = { ...norm_m };
+      this.urgent_manifests = { ...urg_m };
     },
     stopTimer() {
       if (this.interval) {
@@ -104,17 +146,23 @@ export default {
       this.stopTimer();
       this.interval = window.setInterval(() => {
         this.getCountData();
-      }, 600000);
+      }, 300000);
     },
+  },
+  created() {
+    this.getRandom();
   },
   mounted() {
     this.getCountData();
+    /* window.setInterval(() => {
+      this.getRandom();
+    }, 30000); */
   },
 };
 </script>
 
 <style scoped>
-#backlog_col h2{
+#backlog_col h2 {
   font-size: 80px !important;
 }
 </style>
