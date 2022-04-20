c6377e420828e99bebbb55e42bd6ace21db51a20
xanarond <vlakos2895@gmail.com>
2022-04-20 14:27:37 +0300

    Change rows from Inbound Table

--- src/pages/InboundDashboard.vue
+++ src/pages/InboundDashboard.vue
@@ -29,23 +29,21 @@
         />
         <!--        data-type="date" format="dd.MM.yyyy"-->
         <DxScrolling column-rendering-mode="virtual" row-rendering-mode="infinite"/>
-        <DxColumn data-field="Num" alignment="center" caption="Номер"/><!--:group-index="0"-->
+        <DxColumn data-field="client" alignment="center" caption="Клиент"/>
         <DxColumn data-field="type_postavki" alignment="center" caption="Тип поставки"/>
-        <DxColumn :width="200" data-field="plan_date" caption="Планируемая дата"
-                  alignment="center"/>
-        <DxColumn data-field="nomer_truck" caption="Номер" alignment="center"/>
-        <DxColumn data-field="status" caption="Статус" alignment="center"/>
-        <DxColumn data-field="date_startunload" caption="Дата выгрузки" sort-order="desc"
-                  alignment="center"/>
-        <DxColumn :width="170" data-field="time_startunload" caption="Время начала выгрузки" data-type="datetime"
-                  format="H:mm" :allow-filtering="false"
-                  alignment="center"/>
-        <DxColumn :width="200" data-field="time_endunload" caption="Время окончания выгрузки" data-type="datetime"
-                  format="H:mm" :allow-filtering="false"
-                  alignment="center"/>
-        <DxColumn data-field="nomer_postavki" caption="Номер поставки" alignment="center"/>
+        <DxColumn data-field="normativ_v" alignment="center" caption="Норматив выгрузки (из графика)"/><!--:group-index="0"-->
+        <DxColumn data-field="plan_prihoda" alignment="center" caption="План прихода"/>
+        <DxColumn data-field="nomer_truck" alignment="center" caption="Номер ТС"/>
+        <DxColumn data-field="status" alignment="center" caption="Статус выгрузки"/>
+        <DxColumn data-field="unload_do" alignment="center" caption="Выгрузить ДО (из графика)"/>
+        <DxColumn data-field="arrival_datetime" alignment="center" caption="Факт Приезда (из графика)"/>
+        <DxColumn data-field="start_unl_datetime" alignment="center" caption="Факт выгрузки начало (из графика)"/>
+        <DxColumn data-field="fin_unl_datetime" alignment="center" caption="Факт выгрузки конец (из графика)"/>
+        <DxColumn data-field="grade" alignment="center" caption="Оценка работы (из графика)"/>
+        <DxColumn data-field="nomer_postavki" alignment="center" caption="Номер Входящей поставки(из графика)"/>
 <!--        <DxGroupPanel :visible="true"/>-->
 <!--        <DxGrouping :auto-expand-all="autoExpandAll"/>-->
+<!--        data-type="datetime" format="dd/MM/yy. H:mm"-->
         <DxPaging :page-size="30"/>
         <DxPager
             :show-page-size-selector="true"
